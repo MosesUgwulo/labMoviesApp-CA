9740aa33464b7ee2cb2d2b3929d250e78a776498
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2022-11-03 16:16:55 +0000

    Completed sign up page

--- movies-app/src/components/App.js
+++ movies-app/src/components/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React from 'react';
+import SignUp from './signUp';
+import Container from 'react-bootstrap/Container';
+import { AuthProvider } from '../contexts/AuthContext';
 
 function App() {
-  return "Hello World";
-
-  
+  return (
+    <AuthProvider>
+    <Container className='d-flex align-items-center justify-content-center'
+      style={{ minHeight: '100vh' }}
+    >
+      <div className='w-100' style={{ maxWidth: '400px' }}>
+        <SignUp />
+      </div>
+    </Container>
+    </AuthProvider>
+  );
 }
 
 export default App;
